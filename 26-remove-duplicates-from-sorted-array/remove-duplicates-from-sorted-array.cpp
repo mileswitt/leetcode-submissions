92268f300b8f0a79ea825ed56746f6372de74ac5
miles witt <mileswitt0903@gmail.com>
2026-06-03 23:20:06 -0700

    Time: 0 ms (100.00%) | Memory: 22.6 MB (51.02%) - LeetSync

---
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+
+        int counter = 1;
+
+        for (int i = 1; i < nums.size(); i++) {
+            if (nums[i] != nums[i-1]) {
+                nums[counter] = nums[i];
+                counter++;
+            }
+        }
+        return counter;
+        
+    }
+};
