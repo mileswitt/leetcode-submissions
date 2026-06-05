c5d335041b9b1a40dc6a56a7d9d27b6d1c026551
miles witt <mileswitt0903@gmail.com>
2026-06-04 22:39:09 -0700

    Time: 3 ms (34.03%) | Memory: 11.8 MB (45.80%) - LeetSync

--- 412-fizz-buzz/fizz-buzz.cpp
+++ 412-fizz-buzz/fizz-buzz.cpp
@@ -2,17 +2,17 @@ class Solution {
 public:
     vector<string> fizzBuzz(int n) {
 
-        vector<string> answer(n, "");
+        vector<string> answer;
 
         for (int i = 1; i <= n; i++) {
             if (i % 3 == 0 && i % 5 == 0) {
-                answer[i - 1] = "FizzBuzz";
+                answer.push_back("FizzBuzz");
             } else if (i % 3 == 0) {
-                answer[i - 1] = "Fizz";
+                answer.push_back("Fizz");
             } else if (i % 5 == 0) {
-                answer[i - 1] = "Buzz";
+                answer.push_back("Buzz");
             } else {
-                answer[i - 1] = to_string(i);
+                answer.push_back(to_string(i));
             }
         }
         return answer;
